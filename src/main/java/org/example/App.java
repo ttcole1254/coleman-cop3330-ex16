e274fbef1c98a80fe6872ae83c8ad0caa4bb08f9
Test <Test@test.com>
2021-09-13 22:13:26 -0400

    Added assignment code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,21 @@
 package org.example;
 
-/**
- * Hello world!
- *
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 16 Solution
+ *  Copyright 2021 Tyler Coleman
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is your age? ");
+        String ageS = in.nextLine();
+        int age = parseInt(ageS);
+        System.out.println(age < 16 ? "You are not old enough to legally drive." : "You are old enough to legally drive.");
     }
 }
